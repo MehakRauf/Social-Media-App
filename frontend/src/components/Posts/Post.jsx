5c76d75c37e6d602afbe623a3f3c7626d00998ba
Mehak Fatima <mehak.fatima2386@gmail.com>
2024-12-26 20:56:14 +0500

    done with stories and post section designing

--- frontend/src/components/Posts/Post.jsx
+++ frontend/src/components/Posts/Post.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './Post.scss';
+import Posts from '../post/Posts';
 
 const Post = () => {
+    const posts = [
+        {
+            id: 1,
+            name: "Mehak Fatima",
+            userId: 1,
+            profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-yi8lUjKNVqmWkbY2plqdttxFpzC2Efcq0g&s",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, error?",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBxc6LifmVV2y_hUNP6RvotH2sd4ljWoHyuQ&s"
+        },
+        {
+            id: 2,
+            name: "Mehak Fatima",
+            userId: 2,
+            profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-yi8lUjKNVqmWkbY2plqdttxFpzC2Efcq0g&s",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, error?",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBxc6LifmVV2y_hUNP6RvotH2sd4ljWoHyuQ&s"
+        }
+
+    ]
     return (
-        <div>Post</div>
+        <div className="posts">
+            <div>
+                {posts.map(post => (
+                    <Posts post={post} key={post.id} />
+                ))}
+            </div>
+        </div>
     )
 }
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className='home'>
       <Stories />
-      <Share />
+      {/* <Share /> */}
       <Post />
     </div>
   )
